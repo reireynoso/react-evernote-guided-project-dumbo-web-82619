24b4a6ece0c37035c1bc0f2d71744ffb86f2582b
lsamano <leizl.samano@gmail.com>
2019-11-09 22:02:23 -0500

    search added

--- frontend/src/components/NoteContainer.js
+++ frontend/src/components/NoteContainer.js
@@ -6,7 +6,8 @@ import Content from './Content';
 class NoteContainer extends Component {
   state = {
     allNotes: [],
-    chosenNote: {}
+    chosenNote: {},
+    searchTerm: ""
   }
 
   componentDidMount() {
@@ -33,12 +34,24 @@ class NoteContainer extends Component {
     })
   }
 
+  handleSearchTermChange = event => {
+    this.setState({
+      searchTerm: event.target.value
+    })
+  }
+
+  filterNotes = () => {
+    return this.state.allNotes.filter(note => {
+      return note.title.toLowerCase().includes(this.state.searchTerm.toLowerCase())
+    })
+  }
+
   render() {
     return (
       <Fragment>
-        <Search />
+        <Search searchTerm={this.state.searchTerm} handleSearchTermChange={this.handleSearchTermChange} />
         <div className='container'>
-          <Sidebar allNotes={this.state.allNotes} handleChosenNote={this.handleChosenNote} />
+          <Sidebar allNotes={this.filterNotes()} handleChosenNote={this.handleChosenNote} />
           <Content chosenNote={this.state.chosenNote} patchNoteInAllNotes={this.patchNoteInAllNotes} />
         </div>
       </Fragment>
--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
-const Search = () => {
+const Search = ({ searchTerm, handleSearchTermChange }) => {
   return (
     <div className="filter">
       <input
         id="search-bar"
         type="text"
         placeholder="Search Notes"
+        value={searchTerm}
+        onChange={handleSearchTermChange}
       />
     </div>
   );
