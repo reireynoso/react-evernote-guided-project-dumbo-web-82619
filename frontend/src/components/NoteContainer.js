a18561a8ec87ad545deac371f198557bc3280d55
lsamano <leizl.samano@flatironschool.com>
2019-11-11 09:51:29 -0500

    completed post note

--- frontend/src/components/NoteContainer.js
+++ frontend/src/components/NoteContainer.js
@@ -36,6 +36,29 @@ class NoteContainer extends Component {
     })
   }
 
+  postNoteInAllNotes = newNote => {
+    this.setState({
+      allNotes: [ ...this.state.allNotes, newNote ]
+    })
+  }
+
+  makeNewNote = event => {
+    fetch('http://localhost:3000/api/v1/notes', {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        title: "Default Title",
+        body: "This is the default body.",
+        user_id: 1
+      })
+    })
+      .then(res => res.json())
+      .then(this.postNoteInAllNotes)
+  }
+
   handleSearchTermChange = event => {
     this.setState({
       searchTerm: event.target.value
@@ -65,6 +88,7 @@ class NoteContainer extends Component {
           <Sidebar 
             allNotes={this.filterNotes()} 
             handleChosenNote={this.handleChosenNote}
+            makeNewNote={this.makeNewNote}
            />
           <Content 
             chosenNote={this.state.chosenNote} 
